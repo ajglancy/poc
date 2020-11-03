3a21b479c295159a6a4b7c2cf44e5b89c6c3d8a0
ajglancy <73659572+ajglancy@users.noreply.github.com>
2020-11-03 11:21:04 +0000

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -7,14 +7,7 @@ resource "aws_kms_key" "mykey" {
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "538215634837-testbucket"
   acl    = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+  
   versioning {
     enabled = true
   }
