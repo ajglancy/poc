5ddf10779c404731f82b0f959640e9c9d6b0d7e5
ajglancy <73659572+ajglancy@users.noreply.github.com>
2020-11-04 09:17:32 +0000

    Update s3_test.rb

--- test/unit/s3/s3_test.rb
+++ test/unit/s3/s3_test.rb
@@ -5,32 +5,32 @@ require 'aws-sdk'
 require 'rhcl'
 
 # Test to ensure the S3 bucket has versioning enabled 
-describe s3_bucket('538215634837-testbucket') do
+describe s3_bucket('538215634837-testbucket-poc') do
     it { should have_versioning_enabled }
   end
   
 # Test to ensure the S3 bucket has server side encryption enabled
-  describe s3_bucket('538215634837-testbucket') do
+  describe s3_bucket('538215634837-testbucket-poc') do
     it { should have_server_side_encryption(algorithm: "aws:kms") }
   end
 
 # Test to ensure the S3 bucket has the correct bucket owner 
-  describe s3_bucket('538215634837-testbucket') do
+  describe s3_bucket('538215634837-testbucket-poc') do
     its('resource.acl.owner.display_name') { should eq 'my-bucket-owner' }
   end
 
 # Test to ensure MFA delete is enabled on the bucket  
-  describe s3_bucket('538215634837-testbucket') do
+  describe s3_bucket('538215634837-testbucket-poc') do
     it { should have_mfa_delete_enabled }
   end
 
 # Test to ensure the S3 bucket has logging enabled
-  describe s3_bucket('538215634837testbucket') do
+  describe s3_bucket('538215634837-testbucket-poc') do
     it { should have_logging_enabled(target_bucket: 'my-log-bucket', target_prefix: 'logs/') }
   end
 
 # Test to ensure the S3 file has been successfully uploaded and is encrypted
-  describe s3_bucket('538215634837-testbucket') do
+  describe s3_bucket('538215634837-testbucket-poc') do
     it { should have_object('/test.txt') }
     it { should have_server_side_encryption(algorithm: "aws:kms") }
   end
